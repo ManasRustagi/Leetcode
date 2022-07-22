6267b07008b7e9aa09069137faaa0b5eab550770
Manas Rustagi <60061736+ManasRustagi@users.noreply.github.com>
2022-07-22 16:45:00 +0530

    Time: 887 ms (22.52%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,5 +1,3 @@
 select distinct a.num as ConsecutiveNums
-from Logs a
-where 
-a.num=(select b.num from logs b where b.id>a.id order by b.id limit 1 offset 0) and 
-a.num=(select c.num from logs c where c.id>a.id order by c.id limit 1 offset 1) 
+from Logs a,Logs b, Logs c
+where b.id=a.id+1 and c.id=b.id+1 and a.num=b.num and b.num=c.num;
