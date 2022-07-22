b56f6dbe17af1fbd0ba22f0096e786b436f3a8a6
Manas Rustagi <60061736+ManasRustagi@users.noreply.github.com>
2022-07-22 17:01:19 +0530

    Time: 343 ms (30.72%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 with cte as
-(select*, dense_rank() over(order by salary desc) as R
-from Employee)
-
-select ifnull((select salary from cte where r=2 limit 1),null) as SecondHighestSalary;
+(select distinct salary  from Employee 
+order by salary desc
+limit 1 offset 1)
+select ifnull((select salary from cte),null) as SecondHighestSalary;
