f1dcdfc0e707ef05c36b81b7740830e78d4d6e73
Manas Rustagi <60061736+ManasRustagi@users.noreply.github.com>
2022-08-08 16:09:43 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (72.64%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void Recursion(int index,vector<int> &a,vector<int> nums,int n,vector<vector<int>> &b)
+    void Recursion(int index,vector<int> &a,vector<int> nums,vector<vector<int>> &b)
     {
         b.push_back(a);
-        for(int i=index;i<n;i++)
+        for(int i=index;i<nums.size();i++)
         {
             if(i!=index && nums[i]==nums[i-1])continue;
             a.push_back(nums[i]);
-            Recursion(i+1,a,nums,n,b);
+            Recursion(i+1,a,nums,b);
             a.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
         vector<vector<int>> a;
         vector<int> b;
         sort(nums.begin(),nums.end());
-        Recursion(0,b,nums,n,a);
+        Recursion(0,b,nums,a);
         return a;
     }
 };
