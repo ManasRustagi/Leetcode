12d344ba78ea89840b518e64dd3cb10c2dc7741b
Manas Rustagi <60061736+ManasRustagi@users.noreply.github.com>
2022-08-14 17:25:44 +0530

    Time: 4 ms (66.43%), Space: 7.9 MB (45.39%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void Recursion(int freq[],vector<int> nums,vector<int> & a,vector<vector<int>> &b)
+    {
+        if(a.size()==nums.size())
+        {
+            b.push_back(a);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(!freq[i])
+            {
+                a.push_back(nums[i]);
+                freq[i]=1;
+                Recursion(freq,nums,a,b);
+                freq[i]=0;
+                a.pop_back();
+            }
+        }
+        
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+       vector<int> a;
+        vector<vector<int>> b;
+        int n=nums.size();
+        int freq[n];
+        for(int i=0;i<n;i++) freq[i]=0;
+        Recursion(freq,nums,a,b);
+        return b;
+    }
+};
