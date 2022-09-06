2d7fbb25841a32f85741205fee725cf0b60deb27
Manas Rustagi <60061736+ManasRustagi@users.noreply.github.com>
2022-09-06 22:11:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.64%) - LeetHub

--- 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
+++ 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     double nthPersonGetsNthSeat(int n) {
-        vector<double> dp(2);
-        dp[0] = 1.0;  // zero-indexed
-        for (int i = 2; i <= n; ++i) {
-            dp[(i - 1) % 2] = 1.0 / i + dp[(i - 2) % 2] * (i - 2) / i;
-        }
-        return dp[(n - 1) % 2];
+        return (n<2 ? double(1) : double(1)/double(2));
     }
 };
