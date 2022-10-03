2d8f64bc25de66babc18dd3d10bb005c85fb79fa
Manas Rustagi <60061736+ManasRustagi@users.noreply.github.com>
2022-10-03 12:15:02 +0530

    Time: 31 ms (12.22%), Space: 16.1 MB (82.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -4,8 +4,8 @@ public:
         int start=0,end=nums.size()-1;
         while(start<end)
         {
-            if(start<end && nums[start]%2==0) start++;
-            if(start<end && nums[end]%2!=0) end--;
+            while(start<end && nums[start]%2==0) start++;
+            while(start<end && nums[end]%2!=0) end--;
             swap(nums[start],nums[end]);
         }
         return nums;
